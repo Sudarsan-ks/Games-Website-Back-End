99c49ee5d151c1aba56ee3522247522904b01903
Sudarsan-ks <sansudar908@gmail.com>
2025-02-10 21:22:57 +0530

    update

--- Routers/auth.js
+++ Routers/auth.js
@@ -4,7 +4,7 @@ const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
-      return res.status(404).json({ message: "No token provided" });
+      return res.status(401).json({ message: "No token provided" });
     }
 
     const decoded = jwt.decode(token);
