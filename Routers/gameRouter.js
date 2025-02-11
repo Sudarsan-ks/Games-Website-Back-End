e778ff47ad0dda57eca05da15f26dd3f446678eb
Sudarsan-ks <sansudar908@gmail.com>
2025-02-11 20:03:46 +0530

    update

--- Routers/gameRouter.js
+++ Routers/gameRouter.js
@@ -4,62 +4,65 @@ const Game = require("../Models/gameSchema");
 const { adminAuth, auth } = require("./auth");
 
 module.exports = (io) => {
-  io.on("connection", (socket) => {
-    console.log("A user connected: " + socket.id);
+  if (!io._connectionListenerSet) {
+    io._connectionListenerSet = true;
+    io.on("connection", (socket) => {
+      console.log("A user connected: " + socket.id);
 
-    socket.on("createRoom", async ({ gameType, playerID }) => {
-      const roomId = Math.random().toString(36).substring(2, 10);
-      const game = new Game({ roomId, gameType, players: [playerID] });
-      await game.save();
-      socket.join(roomId);
-      socket.emit("roomCreated", { roomId, game });
-    });
-
-    socket.on("joinRoom", async ({ roomId, playerID }) => {
-      let game = await Game.findOne({ roomId });
-      if (!game) {
-        return socket.emit("error", "Room not found");
-      }
-
-      if (game.players.length < 2) {
-        game.players.push(playerID);
+      socket.on("createRoom", async ({ gameType, playerID }) => {
+        const roomId = Math.random().toString(36).substring(2, 10);
+        const game = new Game({ roomId, gameType, players: [playerID] });
         await game.save();
         socket.join(roomId);
-        io.to(roomId).emit("playerJoined", { players: game.players });
-      } else {
-        socket.emit("error", "Room is full");
-      }
-    });
+        socket.emit("roomCreated", { roomId, game });
+      });
 
-    socket.on("gameMove", ({ roomId, move }) => {
-      io.to(roomId).emit("updateGame", move);
-    });
-
-    socket.on("declareWinner", async ({ roomId, winnerID }) => {
-      let game = await Game.findOne({ roomId });
-      if (game) {
-        game.winner = winnerID;
-        await game.save();
-        io.to(roomId).emit("gameOver", { winner: winnerID });
-      }
-    });
-
-    socket.on("disconnect", async () => {
-      console.log("A user disconnected: " + socket.id);
-
-      const game = await Game.findOne({ players: socket.id });
-      if (game) {
-        game.players = game.players.filter((player) => player !== socket.id);
-        if (game.players.length === 0) {
-          await Game.deleteOne({ _id: game._id });
-        } else {
-          await game.save();
-          io.to(game.roomId).emit("playerLeft", { players: game.players });
+      socket.on("joinRoom", async ({ roomId, playerID }) => {
+        let game = await Game.findOne({ roomId });
+        if (!game) {
+          return socket.emit("error", "Room not found");
         }
-      }
-    });
-  });
 
+        if (game.players.length < 2) {
+          game.players.push(playerID);
+          await game.save();
+          socket.join(roomId);
+          io.to(roomId).emit("playerJoined", { players: game.players });
+        } else {
+          socket.emit("error", "Room is full");
+        }
+      });
+
+      socket.on("gameMove", ({ roomId, move }) => {
+        io.to(roomId).emit("updateGame", move);
+      });
+
+      socket.on("declareWinner", async ({ roomId, winnerID }) => {
+        let game = await Game.findOne({ roomId });
+        if (game) {
+          game.winner = winnerID;
+          await game.save();
+          io.to(roomId).emit("gameOver", { winner: winnerID });
+        }
+      });
+
+      socket.on("disconnect", async () => {
+        console.log("A user disconnected: " + socket.id);
+
+        const game = await Game.findOne({ players: socket.id });
+        if (game) {
+          game.players = game.players.filter((player) => player !== socket.id);
+          if (game.players.length === 0) {
+            await Game.deleteOne({ _id: game._id });
+          } else {
+            await game.save();
+            io.to(game.roomId).emit("playerLeft", { players: game.players });
+          }
+        }
+      });
+    });
+  }
+  
   router.get("/gamesDetatils", auth, adminAuth, async (req, res) => {
     try {
       const games = await Game.find().populate("players").populate("winner");
--- index.js
+++ index.js
@@ -10,24 +10,13 @@ const gameRouter = require("./Routers/gameRouter");
 
 const app = express();
 const PORT = process.env.PORT;
-
-const server = http.createServer(app);
-const io = socketIO(server, {
-  cors: {
-    origin: [process.env.CLIENT_URL, process.env.CLIENT_URL_NETLIFY],
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-  transports: ["websocket", "polling"],
-});
-
 const corsOptions = {
   origin: (origin, callback) => {
     const allowedOrigins = [
       process.env.CLIENT_URL,
       process.env.CLIENT_URL_NETLIFY,
     ];
-    if (allowedOrigins.includes(origin) || !origin) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       console.log(`Blocked by CORS: ${origin}`);
@@ -42,6 +31,18 @@ const corsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
+const server = http.createServer(app);
+
+const io = socketIO(server, {
+  cors: {
+    origin: [process.env.CLIENT_URL, process.env.CLIENT_URL_NETLIFY],
+    methods: ["GET", "POST"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  },
+  transports: ["websocket", "polling"],
+});
+
 app.use("/user", userRouter);
 app.use("/game", gameRouter(io));
 
