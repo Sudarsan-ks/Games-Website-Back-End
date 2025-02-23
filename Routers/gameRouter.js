04e851ebf78e5ee4a6e576d77469ae41b04be2f3
Sudarsan-ks <sansudar908@gmail.com>
2025-02-23 17:34:18 +0530

    update

--- Routers/gameRouter.js
+++ Routers/gameRouter.js
@@ -101,7 +101,7 @@ module.exports = (io) => {
   router.get("/roomDetail/:roomId", auth, async (req, res) => {
     try {
       const { roomId } = req.params;
-      const room = await Game.findOne({ roomId }).populate("user");
+      const room = await Game.findOne({ roomId }).populate("players");
       res.status(200).json(room);
     } catch (error) {
       res.status(500).json({ error: "Error fetching game details" });
